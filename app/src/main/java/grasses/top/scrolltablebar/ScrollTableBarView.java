da4b5bf77517eca1566e6a38ad0fce11526289f8
grasses <2458857555@qq.com>
2023-10-20 20:30:37 +0800

    create ScrollTableVarView

---
+++ app/src/main/java/grasses/top/scrolltablebar/ScrollTableBarView.java
@@ -0,0 +1,21 @@
+package grasses.top.scrolltablebar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class ScrollTableBarView extends View {
+    public ScrollTableBarView(Context context) {
+        super(context);
+    }
+
+    public ScrollTableBarView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScrollTableBarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
